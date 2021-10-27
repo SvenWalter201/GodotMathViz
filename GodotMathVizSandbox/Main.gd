a469f7b718322f5fea0c10f0fb3cf6e1fcc727f1
SvenWalter201 <sven@wa-soft.de>
2021-10-27 21:57:34 +0200

    changed project structure, to enable smooth editing in Visual Studio

--- GodotMathVizSandbox/Main.gd
+++ GodotMathVizSandbox/Main.gd
@@ -4,12 +4,6 @@ var pfSolver
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pfSolver = load("res://bin/pathfindingsolver.gdns").new()
-	pfSolver.GenerateLevelGrid()
-
-func _on_Sprite_position_changed(node, new_pos):
-	print("The position of " + node.name + " is now " + str(new_pos))
-	var path = pfSolver.FindPath(Vector2(0,0), Vector2(3,4))
-	print(path)
+	pass
 	
 	
